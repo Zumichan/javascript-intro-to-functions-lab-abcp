b973242607179bbf5623b245b0e3d9a8cfaab088
Azumi Callanan <azucm@hotmail.com>
2018-01-13 17:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string === string.toLowerCase){
     return ("I can't hear you!");
+  }else if(string === string.toUpperCase){
+    return ("YES INDEED!");
+  }else if (string === {
+    return "I love you, too."
   }
-}
 
-function sayHiToGrandma(string){
-  if(string === string.toUpperCase){
-    return ();
-  }
-}
